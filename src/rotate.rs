91ba6094bafd444fa4092f5498a9a33d184e2db9
ilya-rb <ilya.ryabchuk@gmail.com>
2022-04-08 21:06:08 +0300

    Added rotate_cloned

--- src/rotate.rs
+++ src/rotate.rs
@@ -33,6 +33,23 @@ pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
     }
 }
 
+fn rotate_cloned(matrix: &mut Vec<Vec<i32>>) {
+    let size = matrix.len();
+    let mut rotated = matrix.clone();
+
+    for i in 0..size {
+        for j in 0..size {
+            rotated[j][i] = matrix[size - i - 1][j];
+        }
+    }
+
+    for i in 0..size {
+        for j in 0..size {
+            matrix[i][j] = rotated[i][j];
+        }
+    }
+}
+
 fn swap(i: (usize, usize), j: (usize, usize), matrix: &mut Vec<Vec<i32>>) {
     let tmp = matrix[i.0][i.1];
     matrix[i.0][i.1] = matrix[j.0][j.1];
@@ -41,7 +58,7 @@ fn swap(i: (usize, usize), j: (usize, usize), matrix: &mut Vec<Vec<i32>>) {
 
 #[cfg(test)]
 mod tests {
-    use crate::rotate::rotate;
+    use crate::rotate::{rotate, rotate_cloned};
     use rstest::rstest;
 
     #[rstest(input, expected,
@@ -56,6 +73,22 @@ mod tests {
     )]
     fn test_rotate(input: &mut Vec<Vec<i32>>, expected: &mut Vec<Vec<i32>>) {
         rotate(input);
+
+        assert_eq!(expected, input);
+    }
+
+    #[rstest(input, expected,
+    case(
+    & mut vec ! [ vec ! [1, 2, 3], vec ! [4, 5, 6], vec ! [7, 8, 9] ],
+    & mut vec ! [ vec ! [7, 4, 1], vec ! [8, 5, 2], vec ! [9, 6, 3] ],
+    ),
+    case(
+    & mut vec ! [ vec ! [5, 1, 9, 11], vec ! [2, 4, 8, 10], vec ! [13, 3, 6, 7], vec ! [15, 14, 12, 16] ],
+    & mut vec ! [ vec ! [15, 13, 2, 5], vec ! [14, 3, 4, 1], vec ! [12, 6, 8, 9], vec ! [16, 7, 10, 11] ],
+    ),
+    )]
+    fn test_rotate_cloned(input: &mut Vec<Vec<i32>>, expected: &mut Vec<Vec<i32>>) {
+        rotate_cloned(input);
 
         assert_eq!(expected, input);
     }
