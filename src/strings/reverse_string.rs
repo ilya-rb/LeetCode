ea6ba9cad967c027c5aeadcd68b1010ce25ddae4
ilya-rb <ilya.ryabchuk@gmail.com>
2022-04-08 21:31:14 +0300

    reverse_string.rs

--- src/strings/reverse_string.rs
+++ src/strings/reverse_string.rs
@@ -15,7 +15,7 @@ pub fn reverse_string(s: &mut Vec<char>) {
     let mut i = 0;
     let mut j = s.len() - 1;
 
-    while i < j {
+    for _ in 0..s.len() / 2 {
         s.swap(i, j);
         i += 1;
         j -= 1;
